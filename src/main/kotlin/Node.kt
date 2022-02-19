d1724dedcdc63298a58f80f781311189bdc06911
Sharan Salian <sharansalian.dev@gmail.com>
2022-02-19 21:50:22 +0530

    Add Node.kt

---
+++ src/main/kotlin/Node.kt
@@ -0,0 +1,17 @@
+class Node<T>(var value: T, var next: Node<T>? = null) {
+    override fun toString(): String {
+        return if (next != null) {
+            "$value -> ${next.toString()}"
+        } else "$value"
+    }
+}
+
+fun main() {
+    val node1 = Node(1)
+    val node2 = Node(2)
+    val node3 = Node(3)
+    node1.next = node2
+    node2.next = node3
+
+    println(node1)
+}
