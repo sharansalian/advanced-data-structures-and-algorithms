abb25c12d38bde40d483b238a3747d784a529c50
Sharan Salian <sharansalian.dev@gmail.com>
2022-02-19 21:44:57 +0530

    Add multiplication board

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
 fun main(args: Array<String>) {
     println("Hello World!")
+    multiplicationBoard(3)
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+}
+
+fun multiplicationBoard(size: Int) {
+    for (number in 1..size) {
+        print(" | ")
+        for (otherNumber in 1..size) {
+            print(" $number x $otherNumber = ${number * otherNumber} | ")
+        }
+        println()
+    }
 }
