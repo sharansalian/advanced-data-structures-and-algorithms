43367604555239cb53b0eb79361cd0dfb4a3cfc4
Sharan Salian <sharansalian.dev@gmail.com>
2022-05-08 15:56:02 +0530

    Add Brute Force function

--- src/main/kotlin/leetcode/Two-Sum.kt
+++ src/main/kotlin/leetcode/Two-Sum.kt
@@ -13,6 +13,19 @@ class Solution {
         }
         return intArrayOf()
     }
+
+    fun twoSumBruteForce(nums: IntArray, target: Int): IntArray? {
+        for (i in nums.indices) {
+            for (j in i + 1 until nums.size) {
+                if (nums[j] == target - nums[i]) {
+                    return intArrayOf(i, j)
+                }
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null
+    }
+
 }
 
 fun main() {
