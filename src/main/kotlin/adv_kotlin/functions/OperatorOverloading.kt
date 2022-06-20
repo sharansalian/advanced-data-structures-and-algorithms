a52909eea02b2c0067e97aabdb7b5c951520885a
Sharan Salian <sharansalian.dev@gmail.com>
2022-06-20 10:55:42 +0530

    Add OperatorOverloading.kt

--- src/main/kotlin/adv_kotlin/functions/OperatorOverloading.kt
+++ src/main/kotlin/adv_kotlin/functions/OperatorOverloading.kt
@@ -1,5 +1,7 @@
 package adv_kotlin.functions
 
+import kotlin.text.StringBuilder
+
 class Time(val hours: Int, val minutes: Int) {
 
     operator fun plus(time: Time): Time {
@@ -8,6 +10,16 @@ class Time(val hours: Int, val minutes: Int) {
     }
 }
 
+operator fun StringBuilder.plus(sb: StringBuilder) {
+    sb.forEach { this.append(it) }
+}
+
 fun main() {
     val time = Time(10, 40) + Time(10, 30)
+
+    val sb = StringBuilder()
+    sb.append("Valhala")
+    for (s in sb){
+        s + "$"
+    }
 }
