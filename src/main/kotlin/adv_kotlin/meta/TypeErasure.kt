74156dd2e76bbba440a9420716d35de1da483468
Sharan Salian <sharansalian.dev@gmail.com>
2022-06-22 08:50:24 +0530

    Add TypeErasure.kt

---
+++ src/main/kotlin/adv_kotlin/meta/TypeErasure.kt
@@ -0,0 +1,13 @@
+package adv_kotlin.meta
+
+fun main() {
+
+    val listOfStrings = listOf("One", "Two", "Three")
+
+    val listNumbers = listOf(1, 2, 3)
+
+}
+
+fun <T> printList(list: List<T>) {
+
+}
