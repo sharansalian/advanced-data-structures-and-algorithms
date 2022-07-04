0816362736877c99f3de0b445273f905476e2467
Sharan Salian <sharansalian.dev@gmail.com>
2022-07-04 11:23:54 +0530

    Add productExceptSelf.kt which does not scale

---
+++ src/main/kotlin/leetcode/hashing/productExceptSelf.kt
@@ -0,0 +1,24 @@
+package leetcode.hashing
+
+fun main() {
+
+    println(productExceptSelf(intArrayOf(0, 0)).contentToString())
+}
+
+fun productExceptSelf(nums: IntArray): IntArray {
+    val res = IntArray(nums.size)
+    val map = hashMapOf<Int, Int>()
+
+    for (i in 0 until nums.size) {
+        map[i] = nums[i]
+    }
+
+    for (i in 0 until nums.size) {
+        val others = map.filter { it.key != i }.map {
+            it.value
+        }.reduce { acc, i -> acc * i }
+        res[i] = others
+    }
+
+    return res
+}
