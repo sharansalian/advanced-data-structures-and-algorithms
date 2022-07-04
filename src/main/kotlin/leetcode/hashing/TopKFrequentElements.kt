537813e089ce8bd0778fcbe56420c12a38697a13
Sharan Salian <sharansalian.dev@gmail.com>
2022-07-04 06:05:01 +0530

    Add topKFrequent

---
+++ src/main/kotlin/leetcode/hashing/TopKFrequentElements.kt
@@ -0,0 +1,30 @@
+package leetcode.hashing
+
+fun main() {
+    println(topKFrequent(intArrayOf(1, 1, 1, 2, 2, 3), 2).contentToString())
+}
+
+fun topKFrequent(nums: IntArray, k: Int): IntArray {
+    val count = hashMapOf<Int, Int>()
+    val freq = MutableList<MutableList<Int>>(nums.size + 1) { mutableListOf() }
+    for (n in nums) {
+        count[n] = count.getOrDefault(n, 0) + 1
+    }
+
+    for ((n, c) in count) {
+        freq[c].add(n)
+    }
+
+    val res = mutableListOf<Int>()
+
+    for (i in freq.size - 1 downTo 0) {
+        for (n in freq[i]) {
+            res.add(n)
+            if (res.size == k) {
+                return res.toIntArray()
+            }
+        }
+    }
+
+    return res.toIntArray()
+}
