5ba8127cf37f6cecbe412391a357e124d671e585
Sharan Salian <sharansalian.dev@gmail.com>
2022-07-15 09:48:44 +0530

    Add next permutation

--- src/main/kotlin/leetcode/arrays_and_hashing/Array.kt
+++ src/main/kotlin/leetcode/arrays_and_hashing/Array.kt
@@ -7,3 +7,20 @@ fun main() {
         println("${a[i+1]}")
     }
 }
+
+
+internal class Solution {
+    fun maxSubArray(nums: IntArray): Int {
+        var maxSubarray = Int.MIN_VALUE
+
+        for (i in nums.indices) {
+            var currentSubarray = 0
+            for (j in i until nums.size) {
+                currentSubarray += nums[j]
+                maxSubarray = Math.max(maxSubarray, currentSubarray)
+            }
+        }
+
+        return maxSubarray
+    }
+}
